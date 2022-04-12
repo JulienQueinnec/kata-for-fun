33d0ef5d9df2424f26aa9744947554316485f4cc
Julien Queinnec <jqueinnec@oxiane.com>
2022-04-12 18:14:02 +0200

    5th test green (input =6)

--- src/main/java/com/oxiane/kata/kataforfun/service/KataForFunService.java
+++ src/main/java/com/oxiane/kata/kataforfun/service/KataForFunService.java
@@ -2,12 +2,26 @@ package com.oxiane.kata.kataforfun.service;
 
 public class KataForFunService {
     public String convert(int input) {
-        if (input == 3) {
-            return "KataKata";
+        StringBuilder sb = new StringBuilder();
+
+        if (input % 3 == 0) {
+            sb.append("Kata");
         }
-        if (input == 5) {
-            return "ForFor";
+        if (input % 5 == 0) {
+            sb.append("For");
         }
-        return ""+input;
+        String inputString = "" + input;
+        if (inputString.contains("3")) {
+            sb.append("Kata");
+        }
+        if (inputString.contains("5")) {
+            sb.append("For");
+        }
+        if (sb.isEmpty()) {
+            return "" + input;
+        } else {
+            return sb.toString();
+        }
+
     }
 }
