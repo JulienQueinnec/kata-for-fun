ae04eb8373443609be6fe76c02ee649c22471de9
Julien Queinnec <jqueinnec@oxiane.com>
2022-04-12 18:28:20 +0200

    Refactoring after 5th test

--- src/main/java/com/oxiane/kata/kataforfun/service/KataForFunService.java
+++ src/main/java/com/oxiane/kata/kataforfun/service/KataForFunService.java
@@ -4,24 +4,48 @@ public class KataForFunService {
     public String convert(int input) {
         StringBuilder sb = new StringBuilder();
 
-        if (input % 3 == 0) {
-            sb.append("Kata");
-        }
-        if (input % 5 == 0) {
-            sb.append("For");
-        }
-        String inputString = "" + input;
-        if (inputString.contains("3")) {
-            sb.append("Kata");
-        }
-        if (inputString.contains("5")) {
-            sb.append("For");
-        }
+        sb.append(isDivisibleBy3Rule(input))
+          .append(isDivisibleBy5Rule(input))
+          .append(contains3Rule(input))
+          .append(contains5Rule(input));
+
+        return checkIfDefaultRule(input, sb);
+    }
+
+    private String checkIfDefaultRule(int input, StringBuilder sb) {
         if (sb.isEmpty()) {
             return "" + input;
         } else {
             return sb.toString();
         }
-
     }
+
+    private String isDivisibleBy3Rule(int input) {
+        if (input % 3 == 0) {
+            return "Kata";
+        }
+        return "";
+    }
+
+    private String isDivisibleBy5Rule(int input) {
+        if (input % 5 == 0) {
+            return "For";
+        }
+        return "";
+    }
+
+    private String contains3Rule(int input) {
+        if (("" + input).contains("3")) {
+            return "Kata";
+        }
+        return "";
+    }
+
+    private String contains5Rule(int input) {
+        if (("" + input).contains("5")) {
+            return "For";
+        }
+        return "";
+    }
+
 }
