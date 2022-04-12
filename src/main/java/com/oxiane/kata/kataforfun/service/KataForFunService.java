d02b503473d5e1c4ff68a5b5c45e0a6ec75cb893
Julien Queinnec <jqueinnec@oxiane.com>
2022-04-12 18:02:02 +0200

    3rd test green

--- src/main/java/com/oxiane/kata/kataforfun/service/KataForFunService.java
+++ src/main/java/com/oxiane/kata/kataforfun/service/KataForFunService.java
@@ -2,6 +2,9 @@ package com.oxiane.kata.kataforfun.service;
 
 public class KataForFunService {
     public String convert(int input) {
+        if (input == 3) {
+            return "KataKata";
+        }
         return ""+input;
     }
 }
